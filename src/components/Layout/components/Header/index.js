e73d80e5c6d4bcf9b91209aa0d3ee53f9a691572
Stanlee <ngoclm@arrowhitech.co>
2022-07-21 18:07:09 +0700

    Create Base React App

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,19 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <>Header</>;
+    return (
+        <>
+            <header className={cx('header-wrapper')}>
+                <div className={cx('container')}>
+                    <h1 className={cx('heading')}>Logo</h1>
+                    <h1 className={cx('heading')}>Search</h1>
+                </div>
+            </header>
+        </>
+    );
 }
 
 export default Header;
